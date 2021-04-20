56b91bff43388d97dbae7d8ad5a0449d825e9fd9
lucasgalarce <galarcelucas@gmail.com>
2021-04-19 21:10:46 -0300

    backend initial config

--- backend/index.js
+++ backend/index.js
@@ -1,70 +1,12 @@
-const { Pool } = require('pg');
+const express = require('express');
+const app = express();
 
-const config = {
-    user: 'postgres',
-    host: 'localhost',
-    password: 'password',
-    database: 'cv-charger-challenge',
-};
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
-const pool = new Pool(config);
+// routes
+app.use(require('./routes/index'));
 
-const getUsers = async () => {
-    try {
-        const users = await pool.query('select * from users');
-        console.log(users.rows)
-        pool.end(); // quitar en prod
-
-    } catch (e) {
-        console.log(e);
-    }
-
-}
-
-const addUser = async () => {
-    try {
-
-        const text = 'INSERT INTO users(username, password) VALUES($1, $2)';
-        const values = ["jon", "jon123"];
-
-        const res = await pool.query(text, values);
-        console.log(res)
-        pool.end(); // quitar en prod
-        
-    } catch (e){
-        console.log(e)
-    }
-}
-
-const deleteUser = async () => {
-    try {
-        const text = 'DELETE FROM users WHERE username = $1';
-        const values = ['jon'];
-
-        const res = await pool.query(text, values);
-        console.log(res)
-        pool.end(); // quitar en prod
-
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-const editUser = async () => {
-    try {
-        const text = 'UPDATE users SET username = $1 WHERE username = $2';
-        const values = ['lucas', 'jon'];
-
-        const res = await pool.query(text, values);
-        console.log(res)
-        pool.end(); // quitar en prod
-        
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-// getUsers();
-// addUser();
-// deleteUser();
-// editUser();
+app.listen(3000)
+console.log('server on port 3000')
